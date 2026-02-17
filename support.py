3e1430d17a7e03faeffae99313e4ff957f9a6996
givanov3-ux <g.ivanov3@g.nsu.ru>
2026-02-17 18:06:46 +0700

    Update support.py

--- support.py
+++ support.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def setup_data() -> dict:
     '''
     TODO:
@@ -13,10 +16,30 @@ def setup_data() -> dict:
 
     Данные поступают из файла setup.txt
     '''
-    pass
+
+    setup = {}
+    with open('setup.txt', 'r', encoding='utf-8') as r:
+        text = r.read().split('\n')
+        r.close()
+
+    for info in text:
+        info_list = info.split(' ')
+
+        num = info_list[0]
+        info_list.remove(info_list[0])
+
+        long = info_list[0]
+        info_list.remove(info_list[0])
+
+        for index in range(len(info_list)):
+            info_list[index] = int(info_list[index][-2:])
+
+        setup[int(num)] = [int(long), info_list]
+
+    return setup
 
 
-def data_for_analyzing() -> list[set]:
+def data_for_analyzing() -> list[tuple]:
     '''
     TODO:
     При моделировании работы автозаправочной станции,
@@ -28,7 +51,18 @@ def data_for_analyzing() -> list[set]:
     Данные поступают из файла input.txt
     На вывод идет список множеств
     '''
-    pass
+
+    with open('input.txt', 'r', encoding='utf-8') as r:
+        text = r.read().split('\n')
+        r.close()
+
+    info = []
+    for item in text:
+        item = item.split(' ')
+        t = datetime.strptime(item[0], '%H:%M')
+        info.append((t, int(item[1]), int(item[2])))
+
+    return info
 
 
 def output_modeling(output_str: str) -> None:
@@ -39,7 +73,11 @@ def output_modeling(output_str: str) -> None:
     :return:
     Вывод в output.txt
     '''
-    pass
+
+    with open('output.txt', 'a') as a:
+        a.write(output_str)
+        a.write('\n')
+        a.close()
 
 
 def oil_price() -> dict:
@@ -51,5 +89,8 @@ def oil_price() -> dict:
     Было бы круто, если цена подгружалась из интернета в реальном времени, но вообще можно просто взять
     и вернуть фиксированные значения.
     Ключ словаря - цифра октанового числа, значение - цена (до 2х знаков после запятой)
-    :return:
+    :return price:
     '''
+
+    price = {80: 54.23, 92: 60.53, 95: 64.97, 98: 85.05}
+    return price
