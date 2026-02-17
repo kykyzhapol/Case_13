acde39ee46d6284f1123ee1fe90a8e48cbc5374d
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2026-02-17 19:08:38 +0700

    Localization rest

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-'''
-В этом файле будет располагаться основная часть кода.
-В файле support
-'''
+from datetime import timedelta, datetime
 import math
 import random
 
+import support as sup
 
-setup_data = {1: [3, [80]], 2: [2, [92]], 3: [4, [92, 95, 98]]}
-analyzing_data = [{'10:36', 15, 80}, {'10:55', 50, 92}, {'10:56', 100, 92}]
-oil_price = {80: 45, 92: 50, 98: 55}
+setup_data = sup.setup_data_input()
+analyzing_data = sup.data_for_analyzing()
+oil_price = sup.fresh_oil_price()
 
 
 def refueling_speed(size: float) -> int:
     '''
-    расчет скорости заправки
-    Средняя скорость заправки: 10 литров в минуту.
-    Однако, эта величина случайным образом может быть увеличена или уменьшена на 1 минуту,
-    либо остаться неизменной.
-    :param size: объем топлива для заправки (литры)
-    :return minutes: время заправки в минутах
+    Calculate refueling time.
+    Average refueling speed: 10 liters per minute.
+    However, this value can be randomly increased or decreased by 1 minute,
+    or remain unchanged.
+
+    Args:
+        size: Fuel volume to refuel (liters)
+
+    Returns:
+        minutes: Refueling time in minutes
     '''
     # Calculate base time (10 liters per minute)
     base_time = math.ceil(size / 10)
@@ -32,7 +34,153 @@ def refueling_speed(size: float) -> int:
     return final_time
 
 
-def main() -> None:
-    for i in range(len(setup_data)):
-        print(f'Автомат {i}, максимальная очередь {setup_data[i][1]}, марка бензина АИ-{setup_data[i][2]}')
+def add_minutes_to_time(t: datetime, minutes: int) -> datetime:
+    '''Add minutes to a datetime object and return a new datetime object'''
+    return t + timedelta(minutes=minutes)
 
+
+def find_available_pump(oil_type, current_queues, setup_data):
+    '''Find a suitable pump for refueling'''
+    available_pumps = []
+
+    for pump_num, pump_info in setup_data.items():
+        max_queue, available_oils = pump_info[0], pump_info[1]
+
+        # Check if the required fuel type is available and if there are free queue slots
+        if oil_type in available_oils:
+            if current_queues[pump_num][0] < max_queue:
+                available_pumps.append(pump_num)
+
+    if available_pumps:
+        # Select the pump with the shortest queue
+        return min(available_pumps, key=lambda x: current_queues[x][0])
+    else:
+        return None
+
+
+def print_pump_status(current_queues, setup_data):
+    '''Display current status of all pumps'''
+    for pump_num, pump_info in setup_data.items():
+        max_queue = pump_info[0]
+        available_oils = pump_info[1]
+        current_queue_length = current_queues[pump_num][0]
+
+        # Create string with fuel grades
+        oils_str = ' '.join([f'АИ-{oil}' for oil in available_oils])
+
+        # Create queue indicator
+        queue_indicator = '*' * current_queue_length
+        out_str = f'Автомат №{pump_num} максимальная очередь: {max_queue} Марки бензина: {oils_str} ->{queue_indicator}'
+        print(out_str)
+        sup.output_modeling(out_str)
+
+
+def main() -> None:
+    # Create dictionary with arrival and departure times for each customer
+    prepared_data = {}
+    for slot in range(len(analyzing_data)):
+        prepared_data[slot] = [*analyzing_data[slot],
+                               add_minutes_to_time(analyzing_data[slot][0],
+                                                   refueling_speed(analyzing_data[slot][1]))]
+
+    # Create dictionary for current queue state
+    # Format: {pump_number: [current_queue_length, max_queue, customer_list]}
+    current_queues = {}
+    for pump_num in setup_data.keys():
+        current_queues[pump_num] = [0, setup_data[pump_num][0], []]  # [current_length, max_length, customer_list]
+
+    # Statistics
+    total_sold = {80: 0, 92: 0, 95: 0, 98: 0}
+    total_revenue = 0
+    rejected_customers = 0
+
+    # List of all events (arrivals and departures)
+    all_events = []
+
+    # Prepare arrival events
+    for car_num, car_data in prepared_data.items():
+        arrival_time, volume, oil_type, departure_time = car_data
+        all_events.append((arrival_time, 'arrival', car_num, volume, oil_type, departure_time))
+
+    # Sort events by time
+    all_events.sort(key=lambda x: x[0])
+
+    # Main simulation loop
+    for event in all_events:
+        event_time, event_type, car_num, volume, oil_type, departure_time = event
+
+        if event_type == 'arrival':
+            # Check if there's a suitable pump available
+            pump_num = find_available_pump(oil_type, current_queues, setup_data)
+
+            if pump_num is not None:
+                # Add customer to queue
+                current_queues[pump_num][0] += 1
+                current_queues[pump_num][1] = setup_data[pump_num][0]  # update max value
+                current_queues[pump_num][2].append({
+                    'car_num': car_num,
+                    'arrival_time': event_time,
+                    'volume': volume,
+                    'oil_type': oil_type,
+                    'departure_time': departure_time
+                })
+
+                out_str = f'\nВ {event_time.strftime('%H:%M')} новый клиент: {event_time.strftime('%H:%M')} АИ-{oil_type} {volume} {car_num + 1} встал в очередь к автомату №{pump_num}'
+                print(out_str)
+                sup.output_modeling(out_str)
+                print_pump_status(current_queues, setup_data)
+
+                # Add departure event
+                all_events.append((departure_time, 'departure', car_num, volume, oil_type, pump_num))
+            else:
+                # Customer leaves
+                rejected_customers += 1
+                out_str = f'\nВ {event_time.strftime('%H:%M')} клиент: {event_time.strftime('%H:%M')} АИ-{oil_type} {volume} {car_num + 1} не смог заправиться (все очереди заняты) и уехал'
+                print(out_str)
+                sup.output_modeling(out_str)
+                print_pump_status(current_queues, setup_data)
+
+            # Sort events again after adding new one
+            all_events.sort(key=lambda x: x[0])
+
+        elif event_type == 'departure':
+            # Customer leaves gas station after refueling
+            pump_num = departure_time  # this field now contains pump number
+
+            # Update statistics
+            total_sold[oil_type] = total_sold.get(oil_type, 0) + volume
+            total_revenue += volume * oil_price.get(oil_type, 0)
+
+            # Remove customer from queue
+            for i, customer in enumerate(current_queues[pump_num][2]):
+                if customer['car_num'] == car_num:
+                    current_queues[pump_num][2].pop(i)
+                    current_queues[pump_num][0] -= 1
+                    break
+            out_str = f'\nВ {event_time.strftime('%H:%M')} клиент {prepared_data[car_num][0].strftime('%H:%M')} АИ-{oil_type} {volume} {car_num + 1} заправил свой автомобиль и покинул АЗС'
+            print(out_str)
+            sup.output_modeling(out_str)
+            print_pump_status(current_queues, setup_data)
+
+    # Display final statistics
+    print('\n' + '=' * 50,
+          'ИТОГОВАЯ СТАТИСТИКА ЗА СУТКИ:',
+          '=' * 50)
+    sup.output_modeling('ИТОГОВАЯ СТАТИСТИКА ЗА СУТКИ:')
+
+    out_str = '\nКоличество литров, проданное за сутки по каждой марке бензина:'
+    print(out_str)
+    for oil_type, liters in total_sold.items():
+        if liters > 0:
+            out_str = f'АИ-{oil_type}: {liters} л'
+            print(out_str)
+            sup.output_modeling(out_str)
+
+    out_str = (f'\nОбщая сумма продаж за сутки: {total_revenue} руб.'
+               f'\nКоличество клиентов, которые покинули АЗС не заправив автомобиль: {rejected_customers}')
+    print(out_str)
+    sup.output_modeling(out_str)
+
+
+if __name__ == '__main__':
+    main()
--- support.py
+++ support.py
@@ -1,55 +1,96 @@
-def setup_data() -> dict:
+from datetime import datetime
+
+
+def setup_data_input() -> dict:
     '''
-    TODO:
-    Функция для считывания первоначальных данный для моделирования.
-    Пример файла на вход:
-    1 3 АИ-80
-    2 2 АИ-92
-    3 4 АИ-92 АИ-95 АИ-98
+    Function for reading initial data for simulation.
+    Example input file:
+    1 3 AI-80
+    2 2 AI-92
+    3 4 AI-92 AI-95 AI-98
 
-    выход: {1: [3, [80]], 2: [2, [92]], 3: [4, [92, 95, 98]]}
+    Output: {1: [3, [80]], 2: [2, [92]], 3: [4, [92, 95, 98]]}
 
-    АИ-80, АИ-92, АИ-95, АИ-98 - единственные возможные варианты бензина.
+    AI-80, AI-92, AI-95, AI-98 are the only possible fuel grades.
 
-    Данные поступают из файла setup.txt
+    Data is read from setup.txt file
     '''
-    pass
+
+    setup = {}
+    with open('setup.txt', 'r', encoding='utf-8') as r:
+        text = r.read().split('\n')
+        r.close()
+
+    for info in text:
+        info_list = info.split(' ')
+
+        num = info_list[0]
+        info_list.remove(info_list[0])
+
+        long = info_list[0]
+        info_list.remove(info_list[0])
+
+        for index in range(len(info_list)):
+            info_list[index] = int(info_list[index][-2:])
+
+        setup[int(num)] = [int(long), info_list]
+
+    return setup
 
 
-def data_for_analyzing() -> list[set]:
+def data_for_analyzing() -> list[list]:
     '''
-    TODO:
-    При моделировании работы автозаправочной станции,
-    заявки на обслуживание (т.е. приезд автомашин на заправку) поступают из файла input.txt
+    During gas station simulation,
+    service requests (i.e., cars arriving for refueling) are read from input.txt file.
 
-    Файл содержит следующие сведения, разделенные пробелом:
-    <время> <количество литров бензина> <марка бензина>
+    The file contains the following information, space-separated:
+    <time> <liters of fuel> <fuel grade>
 
-    Данные поступают из файла input.txt
-    На вывод идет список множеств
+    Data is read from input.txt file
+    Returns a list of lists
     '''
-    pass
+
+    with open('input.txt', 'r', encoding='utf-8') as r:
+        text = r.read().split('\n')
+        r.close()
+
+    info = []
+    for item in text:
+        item = item.split(' ')
+        t = datetime.strptime(item[0], '%H:%M')
+        info.append([t, int(item[1]), int(item[2])])
+
+    return info
 
 
 def output_modeling(output_str: str) -> None:
     '''
-    TODO:
-    Функция, которая добавляет в файл вывода строку из моделирования
-    :param output_str:
-    :return:
-    Вывод в output.txt
+    Function that appends a simulation output string to the output file
+
+    Args:
+        output_str: String to write to output file
+
+    Returns:
+        None
+
+    Output is written to output.txt
     '''
-    pass
+
+    with open('output.txt', 'a', encoding='UTF-8') as a:
+        a.write(output_str)
+        a.write('\n')
 
 
-def oil_price() -> dict:
+def fresh_oil_price() -> dict:
     '''
-    TODO:
-    При расчетах используется цена бензина, которая должна быть представлена в виде словаря.
-    Установите текущую рыночную стоимость бензина каждой марки.
+    Fuel prices are used in calculations and should be represented as a dictionary.
+    Returns current market price for each fuel grade.
 
-    Было бы круто, если цена подгружалась из интернета в реальном времени, но вообще можно просто взять
-    и вернуть фиксированные значения.
-    Ключ словаря - цифра октанового числа, значение - цена (до 2х знаков после запятой)
-    :return:
+    Dictionary key - octane number, value - price (up to 2 decimal places)
+
+    Returns:
+        price: Dictionary with fuel prices by octane number
     '''
+
+    price = {80: 54.23, 92: 60.53, 95: 64.97, 98: 85.05}
+    return price
